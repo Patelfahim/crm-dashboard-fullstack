c47a746fb1be29150794924786616ead28529def
Patel Fahim <pfahim7869@gmail.com>
2026-03-26 19:18:46 +0530

    added sql url

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,22 +1,22 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || 'crm_dashboard',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || 'Patelfahim@120',
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
-    logging: false,
+const sequelize = new Sequelize(process.env.MYSQL_PUBLIC_URL, {
+  dialect: 'mysql',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
+});
 
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log(`✅ MySQL Connected to ${sequelize.config.host}`);
+    console.log("✅ MySQL Connected");
   } catch (error) {
-    console.error(`❌ MySQL Error: ${error.message}`);
+    console.error("❌ MySQL Error:", error.message);
     process.exit(1);
   }
 };
